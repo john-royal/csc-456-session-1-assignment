6ecf91a845fe19789310535f68c258b4bcafcaee
John Royal <34844819+john-royal@users.noreply.github.com>
2024-03-15 22:05:34 -0400

    feat: require authentication for home page and remove /protected test page

--- src/routes/protected.tsx
+++
@@ -1,9 +0,0 @@
-import { useUser } from "../lib/auth";
-
-export default function ProtectedPage() {
-  const user = useUser();
-
-  return (
-    <p>Hello, {user.email}. If you can see this page, you’re signed in.</p>
-  );
-}
