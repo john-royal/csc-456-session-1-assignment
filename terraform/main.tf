5c03b163e2432b432f92cf4250cf9334c27063b5
Jobanpreet Singh <63566188+programmerjoban@users.noreply.github.com>
2024-05-02 21:47:32 +0000

    configured terraform integration with vercel

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+
+resource "vercel_project" "vercelTerraform" {
+  name      = "paw-terraform"
+  framework = "vite"
+  git_repository = {
+    type = "github"
+    repo = "programmerjoban/csc-456-paw"
+  }
+}
