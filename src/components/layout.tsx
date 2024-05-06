a542b2c65a3bae5beceadeeb4e4a08f557ff62ae
John Royal <34844819+john-royal@users.noreply.github.com>
2024-05-06 18:16:05 -0400

    fix: add provider so toasts are shown

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
 
 import Navbar from "./navbar";
+import { Toaster } from "./ui/sonner";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ export default function Layout() {
     <QueryClientProvider client={queryClient}>
       <Navbar />
       <Outlet />
+      <Toaster />
     </QueryClientProvider>
   );
 }
