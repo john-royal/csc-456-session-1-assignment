e636bb94826b89edef874b4273ff5c107024e82d
BVasquez07 <bvasq07@gmail.com>
2024-05-08 23:30:16 -0400

    working on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,23 +12,22 @@ AWS_REGION="us-east-2"
 echo "Building app"
 $VITE_BUILD_COMMAND
 
-echo "Packaging application into a ZIP file..."
+echo "compressing app into a zip file"
 zip -r $ZIP_FILE $BUILD_DIR
 
 echo "uploading application file to AWS"
 aws s3 cp $ZIP_FILE s3://terraform-state-paw-bvasquez07
 
-echo "Deploying application to Elastic Beanstalk..."
+echo "Deploying to Elastic Beanstalk"
 aws elasticbeanstalk create-application-version \
   --application-name $APPLICATION_NAME \
-  --version-label $(date +%Y%m%d%H%M%S) \
+  --version-label $(date +%Y%m%d%)env \
   --source-bundle S3Bucket="$S3_BUCKET",S3Key="$ZIP_FILE" \
   --region $AWS_REGION
 
 aws elasticbeanstalk update-environment \
   --application-name $APPLICATION_NAME \
   --environment-name $ENVIRONMENT_NAME \
-  --version-label $(date +%Y%m%d%H%M%S)\
+  --version-label $(date +%Y%m%d%)env\
   --region $AWS_REGION
 
-echo "Deployment complete!"
