d5e088e1641c0bc5e8b5836bc84147f053192a9f
BVasquez07 <bvasq07@gmail.com>
2024-05-09 00:28:10 -0400

    updaeted deploy shell script file

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,28 @@ APPLICATION_NAME="vite"
 ENVIRONMENT_NAME="paw-environment"
 ZIP_FILE="paw_app.zip"
 BUILD_DIR="dist"
-VITE_BUILD_COMMAND="npm run build"
 S3_BUCKET="terraform-state-paw-bvasquez07"
 AWS_REGION="us-east-2"
 
-
-
-
 echo "Building app"
-$VITE_BUILD_COMMAND
+npm run build
 
 echo "compressing app into a zip file"
-zip -r $ZIP_FILE $BUILD_DIR
+zip -r paw_app.zip dist
 
 echo "uploading application file to AWS"
-aws s3 cp $ZIP_FILE s3://terraform-state-paw-bvasquez07
+aws s3 cp paw_app.zip s3://terraform-state-paw-bvasquez07
 
 echo "Deploying to elastic beanstalk"
 aws elasticbeanstalk create-application-version \
-  --application-name $APPLICATION_NAME \
+  --application-name vite \
   --version-label $(date +%Y%m%d%)env \
-  --source-bundle S3Bucket="$S3_BUCKET",S3Key="$ZIP_FILE" \
-  --region $AWS_REGION
+  --source-bundle S3Bucket="terraform-state-paw-bvasquez07",S3Key="paw_app.zip" \
+  --region us-east-2
 
 aws elasticbeanstalk update-environment \
-  --application-name $APPLICATION_NAME \
-  --environment-name $ENVIRONMENT_NAME \
+  --application-name vite \
+  --environment-name paw-environment \
   --version-label $(date +%Y%m%d%)env\
-  --region $AWS_REGION
+  --region us-east-2
 
