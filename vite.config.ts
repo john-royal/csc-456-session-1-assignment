d242e3e6e9618c5aebbb9ac4dcd529cdc8f0cdd3
BVasquez07 <bvasq07@gmail.com>
2024-05-13 22:17:44 -0400

    pushing E2E tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
+///<reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import {configDefaults} from "vitest/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage:{
+      reporter: ['text', 'json', 'html'],
+      exclude:[
+        ...configDefaults.exclude,
+        '**\/*.config.*/**',
+        '**\/*.cjs/**',
+        '**\/vite-env.d.ts/**',
+        '**\/spinner.tsx/**',
+        '**\/_index.tsx/**'
+      ]
+    },
+  }
 });
